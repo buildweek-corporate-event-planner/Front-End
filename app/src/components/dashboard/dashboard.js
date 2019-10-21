e5dd2c2e247d0d05787fad7aa8425f00354aaf3f
spenclark <spenclark@hotmail.com>
2019-10-21 15:35:20 -0700

    dashboard + addevent form

---
+++ app/src/components/dashboard/dashboard.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Navbar from './navbar'
+
+const Dashboard = () => {
+
+    return (
+        <>
+        <Navbar />
+        <div>
+        
+        </div>
+        </>
+    )
+}
+
+export default Dashboard
