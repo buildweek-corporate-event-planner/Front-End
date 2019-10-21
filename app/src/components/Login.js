0dc80c65a75a673bd8dc4091bd2d5ec7d6d66c37
spenclark <spenclark@hotmail.com>
2019-10-21 12:24:00 -0700

    oboarding forms + landing

--- app/src/components/Login.js
+++ app/src/components/Login.js
@@ -1,9 +1,45 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
+const Login = () => {
 
-function Login() {
+     const handleSubmit = e => {
+          e.preventDefault();
+          console.log("Submission")
+
+     }
 
      return (
-          <div>Login</div>
+          <div>
+               <div>
+                    <h3>Login to Ivory</h3>
+                    <h4>Login in to access the greatest event planning tool ever created!</h4>
+               </div>
+               <div>
+                    <form onSubmit={handleSubmit}>
+                         <label>Email
+                              <input
+                              name="email"
+                              type="email"
+                              value="email"
+                              placeholder="user@email.com"
+                              />
+                         </label>
+
+                        <label>Password
+                         <input
+                              name="password"
+                              type="password"
+                              value="password"
+                              placeholder="password"
+                              />
+                         </label>
+
+                         <button type="submit">Login</button>
+                         <NavLink to='/'>Return to home.</NavLink>
+                    </form>
+               </div>
+               <small>Copyright (c) 2019 getivory.com, LLC </small>
+          </div>
      )
 }
 
--- app/src/components/Register.js
+++ app/src/components/Register.js
@@ -1,9 +1,65 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
 
-function Register() {
+const Register = () => {
+
+    const handleSubmit = e => {
+          e.preventDefault();
+          console.log("Submission")
+
+     }
 
      return (
-          <div></div>
+          <div>
+               <div>
+                    <h3>Signup for Ivory</h3>
+                    <h4>Signup to access the greatest event planning tool ever created!</h4>
+               </div>
+               <div>
+                    <form onSubmit={handleSubmit}>
+                         <label>Email
+                         <input
+                         name="email"
+                         type="email"
+                         // value="email"
+                         placeholder="user@email.com"
+                         />
+                         </label>
+
+                        <label>Password
+                        <input
+                            name="password"
+                            type="password"
+                         //    value="password"
+                            placeholder="password"
+                         />
+                         </label>
+
+                        <label>Role
+                         <input
+                         name="role"
+                         type="text"
+                         // value="role"
+                         placeholder="user@email.com"
+                         />
+                         </label>
+
+                         
+                        <label>Company
+                         <input
+                         name="company"
+                         type="text"
+                         // value="company"
+                         placeholder="Stripe, Microsoft, ect."
+                         />
+                         </label>
+                         
+                         <button type="submit">Signup</button>
+                         <NavLink to='/'>Return to home.</NavLink>
+                    </form>
+               </div>
+               <small>Copyright (c) 2019 getivory.com, LLC </small>
+          </div>
      )
 }
 
